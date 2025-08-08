57149e43d1888614924f3fe7f0a3ea61ea91b282
ternicinc <allexander.linux@gmail.com>
2025-08-08 12:35:42 +0200

    STDY: Small Rust study.

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,3 +1,15 @@
+pub struct Block {
+    pub height: u32,
+    pub hash: [u8; 32],
+    pub timestamp: u64,
+}
+
+pub const GENESIS_BLOCK: Block = Block {
+    height: 0,
+    hash: [0u8; 32],
+    timestamp: 1680000000,
+};
+
 pub fn print_block() {
-    println!("This is a block");
+    println!("Block height: {}", GENESIS_BLOCK.height);
 }
