bceb6e9c9dba126d24018cd56c268bfd5c9bd78f
ternicinc <allexander.linux@gmail.com>
2025-08-08 13:00:52 +0200

    Some basics of the chain,

--- src/blockchain/chain.rs
+++ src/blockchain/chain.rs
@@ -1,3 +1,24 @@
-pub fn print_chain() {
-    println!("This is the chain");
+use super::block::Block;
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Blockchain {
+            chain: vec![],
+        }
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        self.chain.push(block);
+    }
+
+    pub fn print_chain(&self) {
+        for block in &self.chain {
+            println!("Block height: {}, hash: {:?}, timestamp: {}", 
+                     block.height, block.hash, block.timestamp);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 mod blockchain;
 mod daemon;
 
+// fn main() {
+//     // Print Gensis block info
+//     blockchain::genesis::print_genesis_block();
+
+
+    
+//     daemon::run();
+// }
+
 fn main() {
-    // Print Gensis block info
-    blockchain::genesis::print_genesis_block();
-    daemon::run();
+    let mut blockchain = blockchain::chain::Blockchain::new();
+    blockchain.add_block(blockchain::genesis::GENESIS_BLOCK);
+  //  blockchain.print_chain();
+
+    let new_block = blockchain::block::Block {
+        height: 1,
+        hash: [1u8; 32],
+        timestamp: 1680000001,
+    };
+    blockchain.add_block(new_block);
+    blockchain.print_chain();
 }
